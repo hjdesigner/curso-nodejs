32c3d7578107043a9ff35d745a69cf082ee1ff38
Henrique Rodrigues <hjdesigner23@gmail.com>
2016-12-10 15:42:14 -0200

    Cap.3 Verbeos HTTP

--- casadocodigo/app/infra/ProdutosDAO.js
+++ casadocodigo/app/infra/ProdutosDAO.js
@@ -6,6 +6,10 @@ ProdutosDAO.prototype.lista = function(callback){
   this._connection.query('select * from livros',callback);
 }
 
+ProdutosDAO.prototype.salva = function(livro,callback){
+  this._connection.query('insert into livros set ?',livro,callback);
+}
+
 module.exports = function(){
   return ProdutosDAO;
 }
--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,14 +1,26 @@
 module.exports = function(app){
-  app.get('/produtos', function(req,res){
-    var connection = app.infra.connectionFactory();
-    var produtosBanco = new app.infra.ProdutosDAO(connection);
 
-    produtosBanco.lista(function(erros,resultados){
+  var listaProdutos = function(req,res){
+    var connection = app.infra.connectionFactory();
+    var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+
+    ProdutosDAO.lista(function(erros,resultados){
       res.render('produtos/lista',{lista:resultados});
     });
     connection.end();
-  });
+  }
+
+  app.get('/produtos', listaProdutos);
+
   app.get('/produtos/form',function(req,res){
     res.render('produtos/form');
+  });
+  app.post('/produtos',function(req,res){
+    var livro = req.body;
+    var connection = app.infra.connectionFactory();
+    var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+    ProdutosDAO.salva(livro,function(erros,resultados){
+      res.redirect('/produtos');
+    });
   });
 }
